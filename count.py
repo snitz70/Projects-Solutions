5208a332d558898e75e2c042c6092e1a2ba50786
Brian Snyder <bsnyder@technology4retailers.com>
2016-02-25 12:43:34 -0600

    added docstrings

--- count.py
+++ count.py
@@ -1,8 +1,13 @@
 def vowels(string):
+    """method to return the number of vowels in a string"""
+
     return len([x for x in string if x in 'aeiou'])
 
 
 def vowel_frequency(string):
+    """method to return a dictionary with the vowels and frequency
+    of each"""
+
     counts = {}
     for s in string:
         if s in 'aeiou':
