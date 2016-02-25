f8915afedb820aff97c773e2f3b80558604bd4d6
Brian Snyder <bsnyder@technology4retailers.com>
2016-02-25 12:48:08 -0600

    added more tests

--- tests/test_fizzbuzz.py
+++ tests/test_fizzbuzz.py
@@ -21,9 +21,8 @@ def test_multiple_of_15():
     assert fizzbuzz.multiple_of_fifteen(30) == 'fizzbuzz'
 
 
-def test_combine
-
-
 def test_fizzbuzz():
-    assert fizzbuzz.fizzbuzz(10) == [1,2,'fizz',4,'buzz','fizz',7,8,'fizz','buzz']
+    assert fizzbuzz.fizzbuzz(15) == [1,2,'fizz',4,'buzz','fizz'
+                                    ,7,8,'fizz','buzz',11,'fizz'
+                                    ,13,14,'fizzbuzz']
 
