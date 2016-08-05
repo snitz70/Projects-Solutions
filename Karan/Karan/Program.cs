61abd1cf04d17c11a11c9360e48bc34bca83d7dd
Brian Snyder <bsnyder@technology4retailers.com>
2016-08-05 12:10:20 -0500

    Initial commit for c# solutions

---
+++ Karan/Karan/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Karan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public static class Numbers
+    {
+        public static double CalculatePi(int decimalPlaces)
+        {
+
+            return System.Math.Round(System.Math.PI, decimalPlaces);
+        }
+
+        public static double CalculateE(int decimalPlaces)
+        {
+            return System.Math.Round(System.Math.E, decimalPlaces);
+        }
+    }
+}
---
+++ Karan/karan.UnitTests/UnitTest1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Karan;
+
+namespace karan.UnitTests
+{
+    [TestClass]
+    public class FindPiTest
+    {
+        [TestMethod]
+        public void FindPiTwoDecimalPlaces()
+        {
+            double expectedResult = 3.14;
+            double actualResult = Karan.Numbers.CalculatePi(2);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void FindPiFiveDecimalPlaces()
+        {
+            double expectedResult = 3.14159;
+            double actualResult = Karan.Numbers.CalculatePi(5);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+
+    [TestClass]
+    public class FindeTest
+    {
+        [TestMethod]
+        public void FindEToFiveDecimalPlaces()
+        {
+            double expectedResult = 2.71828;
+            double actualResult = Karan.Numbers.CalculateE(5);
+        }
+    }
+}
